a098c40b084a0d4f98d2d8c6a225f000aca5eb23
Artyem Pelikhov <artyempel@gmail.com>
2018-02-01 02:09:56 +0200

    Init docker-compose file and package.json.

---
+++ server.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const server  = http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+});
+
+server.listen(8080);
